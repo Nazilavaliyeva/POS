1637117ee847ca874ad54bf977e76101cbdd5395
Nazila Valiyeva (Nazilə Vəliyeva) <Nazilaveliyeva94@gmail.com>
2025-07-19 12:10:18 +0400

    Add project files.

---
+++ POS/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace POS
+{
+    partial class Frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_login));
+            this.lblad = new System.Windows.Forms.Label();
+            this.lblsoyad = new System.Windows.Forms.Label();
+            this.lblsifre = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btngiris = new System.Windows.Forms.Button();
+            this.btncixis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblad
+            // 
+            this.lblad.AutoSize = true;
+            this.lblad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblad.Font = new System.Drawing.Font("Poor Richard", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblad.ForeColor = System.Drawing.Color.White;
+            this.lblad.Location = new System.Drawing.Point(12, 9);
+            this.lblad.Name = "lblad";
+            this.lblad.Size = new System.Drawing.Size(47, 26);
+            this.lblad.TabIndex = 0;
+            this.lblad.Text = " Ad";
+            // 
+            // lblsoyad
+            // 
+            this.lblsoyad.AutoSize = true;
+            this.lblsoyad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblsoyad.Font = new System.Drawing.Font("Poor Richard", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsoyad.ForeColor = System.Drawing.Color.White;
+            this.lblsoyad.Location = new System.Drawing.Point(12, 76);
+            this.lblsoyad.Name = "lblsoyad";
+            this.lblsoyad.Size = new System.Drawing.Size(71, 26);
+            this.lblsoyad.TabIndex = 1;
+            this.lblsoyad.Text = "Soyad";
+            // 
+            // lblsifre
+            // 
+            this.lblsifre.AutoSize = true;
+            this.lblsifre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblsifre.Font = new System.Drawing.Font("Poor Richard", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsifre.ForeColor = System.Drawing.Color.White;
+            this.lblsifre.Location = new System.Drawing.Point(12, 143);
+            this.lblsifre.Name = "lblsifre";
+            this.lblsifre.Size = new System.Drawing.Size(59, 26);
+            this.lblsifre.TabIndex = 2;
+            this.lblsifre.Text = "Şifrə";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Poor Richard", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.txtAd.Location = new System.Drawing.Point(12, 38);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(218, 35);
+            this.txtAd.TabIndex = 3;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Font = new System.Drawing.Font("Poor Richard", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoyad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.txtSoyad.Location = new System.Drawing.Point(12, 105);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(218, 35);
+            this.txtSoyad.TabIndex = 4;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Poor Richard", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.txtSifre.Location = new System.Drawing.Point(12, 172);
+            this.txtSifre.Multiline = true;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(218, 35);
+            this.txtSifre.TabIndex = 5;
+            // 
+            // btngiris
+            // 
+            this.btngiris.BackColor = System.Drawing.Color.Green;
+            this.btngiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btngiris.Font = new System.Drawing.Font("Poor Richard", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngiris.ForeColor = System.Drawing.Color.White;
+            this.btngiris.Location = new System.Drawing.Point(12, 213);
+            this.btngiris.Name = "btngiris";
+            this.btngiris.Size = new System.Drawing.Size(218, 39);
+            this.btngiris.TabIndex = 6;
+            this.btngiris.Text = "Giriş";
+            this.btngiris.UseVisualStyleBackColor = false;
+            // 
+            // btncixis
+            // 
+            this.btncixis.BackColor = System.Drawing.Color.Red;
+            this.btncixis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncixis.Font = new System.Drawing.Font("Poor Richard", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncixis.ForeColor = System.Drawing.Color.White;
+            this.btncixis.Location = new System.Drawing.Point(12, 260);
+            this.btncixis.Name = "btncixis";
+            this.btncixis.Size = new System.Drawing.Size(218, 39);
+            this.btncixis.TabIndex = 7;
+            this.btncixis.Text = "Çıxış";
+            this.btncixis.UseVisualStyleBackColor = false;
+            // 
+            // Frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.ClientSize = new System.Drawing.Size(242, 311);
+            this.Controls.Add(this.btncixis);
+            this.Controls.Add(this.btngiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblsifre);
+            this.Controls.Add(this.lblsoyad);
+            this.Controls.Add(this.lblad);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblad;
+        private System.Windows.Forms.Label lblsoyad;
+        private System.Windows.Forms.Label lblsifre;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btngiris;
+        private System.Windows.Forms.Button btncixis;
+    }
+}
+
